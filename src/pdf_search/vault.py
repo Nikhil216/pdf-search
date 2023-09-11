81c3fdd93181935ac668ee3e6bf3ca016536a2f5
Nikhil Devadiga <nmdevadiga216@gmail.com>
2023-09-11 12:16:50 +0530

    Refactored check status of vault in decorator

--- src/pdf_search/vault.py
+++ src/pdf_search/vault.py
@@ -10,15 +10,19 @@ from whoosh import index
 
 from .console import console
 
+def check_status_ok(method):
+    def modified_method(self, *args, **kwargs):
+        if not self.status_ok:
+            raise Exception("Vault has failed to load! Run the method `load_vault`.")
+        return method(self, *args, **kwargs)
+    return modified_method
 
 class Vault:
     def __init__(self, vault_path: str | pathlib.Path):
         self.vault_path = pathlib.Path(vault_path)
-        self.status_ok = self.check_vault_status()
-        if self.status_ok:
-            index_path = self.vault_path / "index"
-            self.file_index = index.open_dir(index_path, "files")
-            self.page_index = index.open_dir(index_path, "pages")
+        self.file_index = None
+        self.page_index = None
+        self.load_vault()
 
     def check_vault_status(self) -> bool:
         if not self.vault_path.exists() or not self.vault_path.is_dir():
@@ -63,28 +67,38 @@ class Vault:
             console.print(f'Created {", ".join(created)}')
         return True
 
+    def load_vault(self):
+        self.status_ok = self.check_vault_status()
+        if not self.status_ok:
+            raise Exception("Failed to load the vault!")
+        index_path = self.vault_path / "index"
+        if not self.file_index:
+            self.file_index = index.open_dir(index_path, "files")
+        if not self.page_index:
+            self.page_index = index.open_dir(index_path, "pages")
+        
+    @check_status_ok
     def write_file_index(self, fields):
         field_names = self.file_index.schema.names()
         invalid_field_names = [name for name in fields.keys() if name not in field_names]
         if invalid_field_names:
             raise ValueError(f"Invalid fields: {', '.join(invalid_field_names)}")
-        if self.status_ok:
-            file_writer = self.file_index.writer()
-            file_writer.add_document(**fields)
-            file_writer.commit()
+        file_writer = self.file_index.writer()
+        file_writer.add_document(**fields)
+        file_writer.commit()
 
+    @check_status_ok
     def write_multiple_page_index(self, pages):
-        if self.status_ok:
-            page_writer = self.page_index.writer()
-            for page_fields in track(pages, "Indexing"):
-                page_writer.add_document(**page_fields)
-            page_writer.commit()
+        page_writer = self.page_index.writer()
+        for page_fields in track(pages, "Indexing"):
+            page_writer.add_document(**page_fields)
+        page_writer.commit()
 
+    @check_status_ok
     def write_page_index(self, page_id, text, url):
-        if self.status_ok:
-            page_writer = self.page_index.writer()
-            page_writer.add_document(id=page_id, text=text, url=url)
-            page_writer.commit()
+        page_writer = self.page_index.writer()
+        page_writer.add_document(id=page_id, text=text, url=url)
+        page_writer.commit()
 
     def get_pdf_url(self, pdf_type, filename) -> str:
         file_path = self.get_pdf_filepath(pdf_type, filename)
@@ -97,69 +111,69 @@ class Vault:
             raise ValueError("pdf_type cannot be None")
         return self.vault_path / pdf_type / filename
 
+    @check_status_ok
     def remove_file_index(self, file_id):
-        if self.status_ok:
-            page_writer = self.page_index.writer()
-            page_writer.delete_by_term("file_id", file_id)
-            page_writer.commit()
-            file_writer = self.file_index.writer()
-            file_writer.delete_by_term("id", file_id)
-            file_writer.commit()
+        page_writer = self.page_index.writer()
+        page_writer.delete_by_term("file_id", file_id)
+        page_writer.commit()
+        file_writer = self.file_index.writer()
+        file_writer.delete_by_term("id", file_id)
+        file_writer.commit()
 
+    @check_status_ok
     def search_pages(self, search_query_str, limit=10):
-        if self.status_ok:
-            page_text_query = QueryParser("text", self.page_index.schema).parse(search_query_str)
-            results = []
-            with self.page_index.searcher() as s:
-                pages = s.search(page_text_query, limit=limit)
-                for page in pages:
-                    results.append(
-                        {
-                            "url": page["url"],
-                            "page_number": page["page_number"],
-                            "file_id": page["file_id"],
-                        }
-                    )
-            file_query_str = " OR ".join(set([page["file_id"] for page in results]))
-            file_title_query = QueryParser("id", self.file_index.schema).parse(file_query_str)
-            file_map = {}
-            with self.file_index.searcher() as s:
-                files = s.search(file_title_query)
-                for file in files:
-                    file_map[file["id"]] = {
-                        "filename": file["filename"],
-                        "pdf_type": file["type"],
+        page_text_query = QueryParser("text", self.page_index.schema).parse(search_query_str)
+        results = []
+        with self.page_index.searcher() as s:
+            pages = s.search(page_text_query, limit=limit)
+            for page in pages:
+                results.append(
+                    {
+                        "url": page["url"],
+                        "page_number": page["page_number"],
+                        "file_id": page["file_id"],
                     }
-            for page in results:
-                page["filename"] = file_map[page["file_id"]]["filename"]
-                page["pdf_type"] = file_map[page["file_id"]]["pdf_type"]
-            return results
+                )
+        file_query_str = " OR ".join(set([page["file_id"] for page in results]))
+        file_title_query = QueryParser("id", self.file_index.schema).parse(file_query_str)
+        file_map = {}
+        with self.file_index.searcher() as s:
+            files = s.search(file_title_query)
+            for file in files:
+                file_map[file["id"]] = {
+                    "filename": file["filename"],
+                    "pdf_type": file["type"],
+                }
+        for page in results:
+            page["filename"] = file_map[page["file_id"]]["filename"]
+            page["pdf_type"] = file_map[page["file_id"]]["pdf_type"]
+        return results
 
+    @check_status_ok
     def search_files(self, query_str, limit=10):
-        if self.status_ok:
-            file_title_query = QueryParser("title", self.file_index.schema).parse(query_str)
-            results = {}
-            with self.file_index.searcher() as s:
-                files = s.search(file_title_query, limit=limit)
-                for file in files:
-                    pdf_type = file["type"]
-                    if pdf_type not in results:
-                        results[pdf_type] = []
-                    results[pdf_type].append(dict(file))
-            return results
+        file_title_query = QueryParser("title", self.file_index.schema).parse(query_str)
+        results = {}
+        with self.file_index.searcher() as s:
+            files = s.search(file_title_query, limit=limit)
+            for file in files:
+                pdf_type = file["type"]
+                if pdf_type not in results:
+                    results[pdf_type] = []
+                results[pdf_type].append(dict(file))
+        return results
 
+    @check_status_ok
     def list_all_files(self):
-        if self.status_ok:
-            file_title_query = QueryParser("type", self.file_index.schema).parse("books OR papers")
-            results = {}
-            with self.file_index.searcher() as s:
-                files = s.search(file_title_query)
-                for file in files:
-                    pdf_type = file["type"]
-                    if pdf_type not in results:
-                        results[pdf_type] = []
-                    results[pdf_type].append(dict(file))
-            return results
+        file_title_query = QueryParser("type", self.file_index.schema).parse("books OR papers")
+        results = {}
+        with self.file_index.searcher() as s:
+            files = s.search(file_title_query)
+            for file in files:
+                pdf_type = file["type"]
+                if pdf_type not in results:
+                    results[pdf_type] = []
+                results[pdf_type].append(dict(file))
+        return results
 
     def nuke(self):
         shutil.rmtree(self.vault_path / "books")
